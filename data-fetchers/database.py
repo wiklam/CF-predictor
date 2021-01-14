21e43c6af555b90f54340331bf3a1b1bb9b0f1c6
Hubert Obrzut <skoczek133@gmail.com>
2021-01-15 00:56:00 +0100

    fixed typo

--- data-fetchers/database.py
+++ data-fetchers/database.py
@@ -201,7 +201,7 @@ def FetchAll():
     handles = FetchUsers()
     contestIds = FetchContests()
     FetchAllContestHistory(handles)
-    FetchAllStandings()
+    FetchAllStandings(contestIds)
     
     
 class Database:
